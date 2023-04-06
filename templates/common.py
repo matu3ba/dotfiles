2594fbe268b9d9edf5b7070449d6fd1bd7a2b1b6
matu3ba <jan.hafer@rwth-aachen.de>
2023-04-06 09:14:23 +0200

    plugin fixup and python dict->str conversion shennanigan

--- templates/common.py
+++ templates/common.py
@@ -404,3 +404,16 @@ def expectEquation(self, is_true: bool, actual: object) -> int:
         return 1
     return 0
 
+# Dict -> str is inconsistent to json -> str, so workaround with
+# dict_asjson_lower = str(dict1).replace("'", '"')
+def combineDictsFromStr():
+  dict1 = {"t1":"val1","t2arr":[{"t2_int":0,"t2_str":"12.0"}], \
+    "t3int":30}
+  dict1_str_raw = str(dict1)
+  # workaround Python shennanigan (mixed " and ' strings are invalid json)
+  dict1_str = dict1_str_raw.replace("'", '"')
+  dict2_str = '{"anotherone":"yes", '
+  dict2_str +=  '"t3int":30,"t4str":'
+  dict2_str += dict1_str + '}'
+  dict2 = json.loads(dict2_str)
+  _ = dict2
