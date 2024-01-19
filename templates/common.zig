43167b7274c6f819f8645a0426dba9a4010d1c3c
Jan Philipp Hafer <anon_1plus1equals3@mailbox.org>
2024-01-19 01:13:03 +0100

    lots of goodies
    * unix system process inspection
    * workaround for pacman broken key behavior
    * analysis of ISA development
    * naive sketch of ideal arcan semantics
    * linux tooling
    * alacritty config yml -> toml

--- templates/common.zig
+++ templates/common.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+// Tooling
+// - kcov https://github.com/liyu1981/kcov
+// - tracy
+// - cooler stuff?
+
 // typesafe version of (u16*)codepoint with codepoint type u8*
 // codepoint created from Utf8View which is [_]u8
 // const char = std.mem.bytesAsValue(u16, codepoint[0..2]); // U+0080...U+07FF
