78d241dc0a1adaff71020761424fe3d81583cdda
Jan Philipp Hafer <anon_1plus1equals3@mailbox.org>
2024-12-02 13:11:11 +0100

    ci: check in scripts

---
+++ ci/aarch64-macos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# aarch64-macos CI script to clean cache, if necessary
+
+# set -x
+set -o errexit # abort on nonzero exitstatus
+set -o nounset # abort on unbound variable
+
+MAX_SIZE_B=2147483648 #2 GB = 2*(1024)**3 B
+ZIG_CACHE_DIR="$(zig env | jq '. "global_cache_dir"')"
+CHECK_SIZE_B=$(du -sb "$ZIG_CACHE_DIR" | cut -f1)
+if test "$CHECK_SIZE_B" -ge $MAX_SIZE_B; then
+  rm -fr "$ZIG_CACHE_DIR"
+fi
+
+zig env
+
+zig build -Dno_opt_deps -Dno_cross test --summary all
---
+++ ci/x86_64-windows.ps1
@@ -0,0 +1,19 @@
+# x86_64-windows CI script to clean cache, if necessary
+function CheckLastExitCode {
+    if (!$?) { exit 1 }
+    return 0
+}
+
+[int] $MAX_SIZE_B = 2147483648 #2 GB = 2*(1024)**3 B
+[string] $ZIG_CACHE_DIR = $(zig env | jq '. "global_cache_dir"')
+[int] $CHECK_SIZE_B = [int]$(gci $ZIG_CACHE_DIR | measure Length -s).Sum
+if ($CHECK_SIZE_B -gt $MAX_SIZE_B) {
+  Remove-Item -Recurse -Force -ErrorAction Ignore -Path $ZIG_CACHE_DIR
+  CheckLastExitCode
+}
+
+zig env
+CheckLastExitCode
+
+zig build -Dno_opt_deps -Dno_cross test --summary all
+CheckLastExitCode
