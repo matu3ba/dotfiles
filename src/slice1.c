74ef8f337270c63ecbe9110a38e004b9fe90862b
Jan Philipp Hafer <anon_1plus1equals3@mailbox.org>
2025-01-08 17:20:22 +0100

    simplify and -Weverything works

--- src/slice1.c
+++ src/slice1.c
@@ -1,5 +1,7 @@
 // zig cc -std=c11 -Werror -Weverything -Wno-unsafe-buffer-usage -Wno-declaration-after-statement -Wno-switch-default -Wno-pre-c11-compat ./src/slice1.c -o slice1.exe && ./slice1.exe
 // zig cc -std=c11 -Werror -Weverything -Wno-unused-macros -Wno-unsafe-buffer-usage -Wno-declaration-after-statement -Wno-switch-default -Wno-pre-c11-compat ./src/slice1.c -o slice1.exe && ./slice1.exe
+// clang -E src/slice1.c
+// clang src/slice1.c -o slice.exe && ./slice1.exe
 
 #include <stddef.h> // size_t
 #include <stdint.h> // int32_t
@@ -11,14 +13,14 @@
     TYPE *buf;                \
   }
 
-#define CINTERVAL \
-  struct {        \
-    size_t start; \
-    size_t end;   \
-  }
+// #define CINTERVAL \
+//   struct {        \
+//     size_t start; \
+//     size_t end;   \
+//   }
 
 // clang-format off
-// vvvvvvvvvvvvvvvv Modify this to adjust fns
+// vvvvvvvvvvvvvvvv Modify this to adjust fns + instantiate CSTR_SLICE_DEFINE
 #define CSTR_MAP_SLICE_TYPES(F, SEP, ...) \
   F (sslice, char, __VA_ARGS__) SEP       \
   F (islice, int, __VA_ARGS__) SEP        \
@@ -28,27 +30,21 @@
 #define CSTR_SLICE_INIT(BUF, LEN) {.buf = (BUF), .len = (LEN)}
 
 
-#define CSTR_SLICE_NEW_GENERATOR_DECL(NAME, TYPE)      \
-    __attribute__((always_inline)) inline cstr_##NAME  \
-        cstr_new_##NAME(TYPE *buf, size_t len)
-
-// before inline cstr_##NAME
-#define CSTR_SLICE_NEW_GENERATOR_IMPL(NAME, TYPE)      \
+#define CSTR_SLICE_GENERATOR_NEW(NAME, TYPE)      \
     __attribute__((always_inline)) inline cstr_##NAME  \
         cstr_new_##NAME(TYPE *buf, size_t len)         \
     {                                                  \
         return (cstr_##NAME)CSTR_SLICE_INIT(buf, len); \
     }
 
-#define CSTR_DEFINE_SLICE(NAME, TYPE)          \
+#define CSTR_SLICE_DEFINE(NAME, TYPE)          \
     typedef CSTR_SLICE_TYPE(TYPE) cstr_##NAME; \
-    CSTR_SLICE_NEW_GENERATOR_DECL(NAME, TYPE); \
-    CSTR_SLICE_NEW_GENERATOR_IMPL(NAME, TYPE)
+    CSTR_SLICE_GENERATOR_NEW(NAME, TYPE)
 
 #define CSTR_DISPATCH_MAP_BASE(STYPE, BTYPE, FUNC) \
   BTYPE * : cstr_##FUNC##_##STYPE
-#define CSTR_DISPATCH_MAP_SLICE(STYPE, BTYPE, FUNC) \
-  cstr_##STYPE : cstr_##FUNC##_##STYPE
+// #define CSTR_DISPATCH_MAP_SLICE(STYPE, BTYPE, FUNC) \
+//   cstr_##STYPE : cstr_##FUNC##_##STYPE
 #define CSTR_DISPATCH_MAP(STYPE, BTYPE, FUNC, MAP_TYPE) \
   CSTR_DISPATCH_MAP_##MAP_TYPE(STYPE, BTYPE, FUNC)
 
@@ -62,24 +58,24 @@
 // Dispatch on base type to call the "new" function with arguments (BUF, LEN)
 #define CSTR_SLICE(BUF, LEN) \
     CSTR_SLICE_DISPATCH(BUF, BASE, new, BUF, LEN)
-// Dispatch on slice type to call "alloc_buffer" with argument LEN
-#define CSTR_ALLOC_SLICE_BUFFER(S, LEN) \
-    CSTR_SLICE_DISPATCH(S, SLICE, alloc_buffer, LEN)
-// Dispatch on slice type to index and sub-slice
-#define CSTR_IDX(S, I) \
-    CSTR_SLICE_DISPATCH(S, SLICE, idx, S, I)
-#define CSTR_SUBSLICE(S, I, J) \
-    CSTR_SLICE_DISPATCH(S, SLICE, subslice, S, I, J)
-#define CSTR_PREFIX(S, I) \
-    CSTR_SLICE_DISPATCH(S, SLICE, prefix, S, I)
-#define CSTR_SUFFIX(S, I) \
-    CSTR_SLICE_DISPATCH(S, SLICE, suffix, S, I)
+// // Dispatch on slice type to call "alloc_buffer" with argument LEN
+// #define CSTR_ALLOC_SLICE_BUFFER(S, LEN) \
+//     CSTR_SLICE_DISPATCH(S, SLICE, alloc_buffer, LEN)
+// // Dispatch on slice type to index and sub-slice
+// #define CSTR_IDX(S, I) \
+//     CSTR_SLICE_DISPATCH(S, SLICE, idx, S, I)
+// #define CSTR_SUBSLICE(S, I, J) \
+//     CSTR_SLICE_DISPATCH(S, SLICE, subslice, S, I, J)
+// #define CSTR_PREFIX(S, I) \
+//     CSTR_SLICE_DISPATCH(S, SLICE, prefix, S, I)
+// #define CSTR_SUFFIX(S, I) \
+//     CSTR_SLICE_DISPATCH(S, SLICE, suffix, S, I)
 // clang-format on
 
 // typedef CSTR_SLICE_TYPE(char) sslice;
-CSTR_DEFINE_SLICE(sslice, char);
-CSTR_DEFINE_SLICE(islice, int);
-CSTR_DEFINE_SLICE(uislice, unsigned int);
+CSTR_SLICE_DEFINE(sslice, char)
+CSTR_SLICE_DEFINE(islice, int)
+CSTR_SLICE_DEFINE(uislice, unsigned int)
 
 void foo(char *str);
 void foo(char *str) {
@@ -94,8 +90,7 @@ int main(void) {
   (void)c_buf;
   (void)i_buf;
 
-  cstr_sslice x;
-  x = CSTR_SLICE(c_buf, 42);
+  cstr_sslice x = CSTR_SLICE(c_buf, 42);
   (void)x;
   // islice y = CSTR_SLICE(i_buf, 42);
 
